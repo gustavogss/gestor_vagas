0d90d1ddc5eb62d85e65748bf92a4d347e6413a3
gustavogss <gustavogss.dev@gmail.com>
2024-04-22 23:45:52 -0300

    feat: first commit :rocket:

---
+++ src/main/java/br/dev/gustavosouza/gestor_vagas/GestorVagasApplication.java
@@ -0,0 +1,13 @@
+package br.dev.gustavosouza.gestor_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/gustavosouza/gestor_vagas/GestorVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.gustavosouza.gestor_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
